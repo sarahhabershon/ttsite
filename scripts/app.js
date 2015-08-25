fe6757c73039375fbfeb28484627c46a1e2f1a50
Sarah Habershon <habershon@gmail.com>
2015-08-26 11:22:04 +1200

    added event handlers for nav links

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,44 @@
-$(function() {
-  console.log("Loaded");
+$(document).ready(function () {
+  $(".link").on("click", function(event) {
+    $(".tabs").find(".active").removeClass("active");
+    $(this).addClass("active");
+    $(".tab").hide();
+    var tab_id = $(this).attr("id");
+    $(tab_id).show();
+  });
 });
 
 
 
 
-
-var theDataAbout = {
-  title: "About",
-  image_url: "/images/teresq.jpg",
-  rightSide: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
-};
-
-var theDataGallery = {
+// $(function() {
+//   console.log("Loaded");
+// });
 
 
-};
-
-var theDataContact = {
-  title: "Contact",
-  image_url: "",
-  leftSide: "",
-  rightSide: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
-};
 
 
-var theTemplateScript = $("#content-template").html();
-var theTemplate = Handlebars.compile(theTemplateScript);
-$(document.body).append(theTemplate(theDataAbout));
+
+// var theDataAbout = {
+//   title: "About",
+//   image_url: "/images/teresq.jpg",
+//   rightSide: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+// };
+
+// var theDataGallery = {
+
+
+// };
+
+// var theDataContact = {
+//   title: "Contact",
+//   image_url: "",
+//   leftSide: "",
+//   rightSide: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+// };
+
+
+// var theTemplateScript = $("#content-template").html();
+// var theTemplate = Handlebars.compile(theTemplateScript);
+// $(document.body).append(theTemplate(theDataAbout));
 
